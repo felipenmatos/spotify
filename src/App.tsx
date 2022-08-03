c553986185846d183dfc208dd192effc8c859b84
Felipe Nonato Carvalho Matos <felipe.matos@awscode.com.br>
2022-08-03 11:14:39 -0300

    fix: schedule parent page responsiveness fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Base } from "./template/base/index";
+import { Header } from "../src/components/Header/index";
 
 function App() {
   return (
-    <div>
-      <Base>Hello World</Base>
-    </div>
+    <Base>
+      <Header />
+      <h1>Hello World</h1>
+    </Base>
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Container } from "./styles";
+import Logo from "../../../assets/image/logo.png";
+
+export function Header() {
+  return <Container></Container>;
+}
